a0c4e9db072de46cee46df344dcee03d59b532ad
Gyanesh Shukla <114917651+shuklaG8@users.noreply.github.com>
2024-09-22 08:33:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Services from './components/Service/Services';
 import RightSide from './components/SeoMarket/RightSide';
 import Form from './components/Form/Form';
+
 
 function App() {
   return (
